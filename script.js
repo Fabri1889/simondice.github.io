ed82f3e1ae6a44634e90073dd27f8fb36c753230
Fabri1889 <131361629+Fabri1889@users.noreply.github.com>
2023-07-20 20:33:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Obtiene referencias a los elementos del DOM
 var round = document.getElementById('round');
 var simonButtons = document.getElementsByClassName('square');
 var startButton = document.getElementById('startButton');
@@ -21,21 +22,26 @@ function Simon(simonButtons, startButton, round) {
   this.highestRound = 0;
   this.score = 0; 
 
+  // Obtenemos el record más alto almacenado en el localstorage
   var storedHighestRound = localStorage.getItem('highestRound');
   if (storedHighestRound) {
     this.highestRound = parseInt(storedHighestRound, 10);
   }
 
+  // Mostramos el record más alto en el elemento de visible
   var recordDisplay = document.getElementById('record');
   recordDisplay.textContent = 'Récord: ' + this.highestRound;
 }
 
+
+// Agregar la función para abrir el formulario de contacto
 function openContactForm() {
   var contactFormSection = document.getElementById('contacto');
   contactFormSection.style.display = 'block';
 
   var overlay = document.getElementById('overlay');
   overlay.style.display = 'block'; 
+}
 
 var contactLink = document.querySelector('a[href="#contacto"]');
 contactLink.addEventListener('click', function(event) {
@@ -43,6 +49,7 @@ contactLink.addEventListener('click', function(event) {
   openContactForm();
 });
 
+// Agregar la función para cerrar el formulario de contacto
 function closeContactForm() {
   var contactFormSection = document.getElementById('contacto');
   contactFormSection.style.display = 'none';
@@ -51,6 +58,7 @@ function closeContactForm() {
   overlay.style.display = 'none'; 
 }
 
+// Cerrar el formulario de contacto al hacer clic en el botón "Enviar"
 var contactForm = document.getElementById('contactForm');
 contactForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -61,10 +69,12 @@ contactForm.addEventListener('submit', function(event) {
     var mailtoLink = 'mailto:tu_correo@example.com' + '?subject=Contacto desde la página web&body=' + encodeURIComponent('Nombre: ' + name + '\nCorreo electrónico: ' + email + '\nMensaje: ' + message);
     window.location.href = mailtoLink;
 
+    
     closeContactForm();
   }
 });
 
+// Función para validar el formulario de contacto
 function validateForm() {
   var name = document.getElementById('name').value;
   var email = document.getElementById('email').value;
@@ -89,10 +99,12 @@ function validateForm() {
   return true;
 }
 
+// Función para validar el formato del correo electrónico
 function isValidEmail(email) {
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
 
 function validateUserName(name) {
   if (name.length >= 3) {
@@ -102,10 +114,14 @@ function validateUserName(name) {
   }
 }
 
+// Inicializamos la instancia de Simon
 Simon.prototype.init = function() {
   var self = this;
+  
   this.display.startButton.onclick = function() {
+    
     userName = prompt('Por favor, ingresa tu nombre de usuario:');
+    
     
     if (validateUserName(userName)) {
       self.startGame();
@@ -115,32 +131,41 @@ Simon.prototype.init = function() {
   };
 };
 
+
+
+
 var simon = new Simon(simonButtons, startButton, round);
 simon.init();
 
+
 Simon.prototype.startGame = function() {
   var self = this;
+  
   this.display.startButton.classList.add('hidden');
   this.display.startButton.disabled = true;
   this.updateRound(0);
   this.userPosition = 0;
   this.sequence = this.createSequence();
+  
   Array.prototype.forEach.call(this.buttons, function(element, i) {
     element.classList.remove('winner');
     element.onclick = function() {
       self.buttonClick(i);
     };
   });
+
   this.showSequence();
   this.startTimer();
   this.updateScoreDisplay();
 };
 
+// Actualizamos el numero de ronda en la pantalla
 Simon.prototype.updateRound = function(value) {
   this.round = value;
   this.display.round.textContent = 'Round ' + this.round;
 };
 
+// Creamos una nueva secuencia de colores
 Simon.prototype.createSequence = function() {
   var self = this;
   return Array.apply(null, new Array(this.totalRounds)).map(function() {
@@ -148,16 +173,19 @@ Simon.prototype.createSequence = function() {
   });
 };
 
+// Obtenemos un color aleatorio
 Simon.prototype.getRandomColor = function() {
   return Math.floor(Math.random() * 4);
 };
 
+// Manejamos el evento click en los botones de colores
 Simon.prototype.buttonClick = function(value) {
   if (!this.blockedButtons) {
     this.validateChosenColor(value);
   }
 };
 
+// Validamos el color elegido por el usuario
 Simon.prototype.validateChosenColor = function(value) {
   if (!this.blockedButtons) {
     if (this.sequence[this.userPosition] === value) {
@@ -176,21 +204,25 @@ Simon.prototype.validateChosenColor = function(value) {
   }
 };
 
+// Actualiza la visualización del puntaje en el elemento del DOM correspondiente
 Simon.prototype.updateScoreDisplay = function() {
   var scoreDisplay = document.getElementById('score');
   scoreDisplay.style.display = 'block'; 
   scoreDisplay.textContent = 'Puntaje: ' + this.score;
 };
 
+// Agregar una función para ocultar el puntaje al inicio
 Simon.prototype.hideScoreDisplay = function() {
   var scoreDisplay = document.getElementById('score');
   scoreDisplay.style.display = 'none';
 };
 
+// Crea una instancia de la clase Simon y la inicializa
 var simon = new Simon(simonButtons, startButton, round);
 simon.init();
-simon.hideScoreDisplay();
+simon.hideScoreDisplay(); // Ocultar el puntaje al inicio
 
+// Verificamos si el juego ha terminado
 Simon.prototype.isGameOver = function() {
   if (this.round === this.totalRounds) {
     this.gameWon();
@@ -200,6 +232,7 @@ Simon.prototype.isGameOver = function() {
   }
 };
 
+// Muestra la secuencia de colores
 Simon.prototype.showSequence = function() {
   var self = this;
   var sequenceIndex = 0;
@@ -217,10 +250,12 @@ Simon.prototype.showSequence = function() {
   }, this.speed);
 };
 
+// Alterna el estilo del boton entre activo y desactivado
 Simon.prototype.toggleButtonStyle = function(button) {
   button.classList.toggle('active');
 };
 
+// El juego se ha perdido
 Simon.prototype.gameLost = function() {
   this.display.startButton.classList.remove('hidden');
   this.display.startButton.disabled = false;
@@ -232,6 +267,7 @@ Simon.prototype.gameLost = function() {
   this.updateScoreDisplay();
 
   if (this.round > this.highestRound) {
+    // Actualiza el récord más alto si la ronda actual es mayor que el récord anterior
     this.highestRound = this.round;
 
     localStorage.setItem('highestRound', this.highestRound.toString());
@@ -240,6 +276,7 @@ Simon.prototype.gameLost = function() {
     recordDisplay.textContent = 'Récord: ' + this.highestRound;
   }
 
+  // Mostrar el modal de "Game Over" y permitir reiniciar el juego
   var gameOverModal = document.getElementById('gameOverModal');
   gameOverModal.style.display = 'block';
 
@@ -251,6 +288,7 @@ Simon.prototype.gameLost = function() {
   });
 };
 
+// Reinicia el juego
 Simon.prototype.resetGame = function() {
   var self = this;
   this.updateRound(0);
@@ -263,6 +301,7 @@ Simon.prototype.resetGame = function() {
   });
 };
 
+// game won
 Simon.prototype.gameWon = function() {
   this.display.startButton.disabled = false;
   this.blockedButtons = true;
@@ -274,6 +313,7 @@ Simon.prototype.gameWon = function() {
   this.updateTimerDisplay();
 };
 
+// Inicia el temporizador
 Simon.prototype.startTimer = function() {
   var self = this;
   this.timerInterval = setInterval(function() {
@@ -282,21 +322,23 @@ Simon.prototype.startTimer = function() {
   }, 1000);
 };
 
+// Detiene el temporizador
 Simon.prototype.stopTimer = function() {
   clearInterval(this.timerInterval);
 };
 
-
+// Actualiza la visualizacion del temporizador
 Simon.prototype.updateTimerDisplay = function() {
   var minutes = Math.floor(this.timerCount / 60);
   var seconds = this.timerCount % 60;
   timerDisplay.textContent = 'Time: ' + minutes + 'm ' + seconds + 's';
 };
 
+// Crea una instancia de la clase Simon y la inicializa
 var simon = new Simon(simonButtons, startButton, round);
 simon.init();
 
-
+// Manejo de eventos del boton de configuracion, modal y modo oscuro
 var configButton = document.getElementById('configIcon');
 var modal = document.getElementById('modalContainer');
 var closeButton = document.getElementsByClassName('close')[0];
@@ -321,6 +363,7 @@ darkModeButton.addEventListener('click', function() {
   body.classList.toggle('dark-mode');
 });
 
+// Manejo de eventos del navar en celulares
 var navbarToggle = document.getElementById('navbar-toggle');
 var navbarMenu = document.getElementById('nav-menu');
 
@@ -328,8 +371,11 @@ navbarToggle.addEventListener('click', function() {
   navbarMenu.classList.toggle('show');
 });
 
+// Función para validar el nombre ingresado
 function isValidName(name) {
   return name.length >= 3;
 }
 
-}
+
+
+
